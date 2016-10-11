0399494d1c80ad3cd4973397c333e97fcf59004d
yuki.m <kd1244161@st.kobedenshi.ac.jp>
2016-10-11 16:50:10 +0900

    helper update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,17 +7,17 @@ module ApplicationHelper
     [
       ["Room:A", "Seat:S1"],
       ["Room:A", "Seat:S2"],
+      ["Room:A", "Seat:S3"],
       ["Room:A", "Seat:A1"],
       ["Room:A", "Seat:A2"],
-      ["Room:A", "Seat:A3"],
       ["Room:B", "Seat:S1"],
       ["Room:B", "Seat:S2"],
+      ["Room:B", "Seat:S3"],
       ["Room:B", "Seat:A1"],
-      ["Room:B", "Seat:A2"],
-      ["Room:B", "Seat:A3"]
+      ["Room:B", "Seat:A2"]
     ][number]
   end
-  
+
   def room_is(number)
     number/10*10+1
   end
